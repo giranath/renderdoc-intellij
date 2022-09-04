84305636088f3af4b1a8609f317722f878a44ac4
GitHub Action <action@github.com>
2022-09-04 13:03:32 +0000

    Template cleanup

--- src/main/kotlin/com/github/giranath/renderdocintellij/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/giranath/renderdocintellij/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.giranath.renderdocintellij.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.giranath.renderdocintellij.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/giranath/renderdocintellij/services/MyProjectService.kt
+++ src/main/kotlin/com/github/giranath/renderdocintellij/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.giranath.renderdocintellij.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.giranath.renderdocintellij.MyBundle
 
 class MyProjectService(project: Project) {
 
